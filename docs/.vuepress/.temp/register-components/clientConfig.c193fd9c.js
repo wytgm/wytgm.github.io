3cb708d8ba1a8f400599449e8fbcff28530901b3
wytgm <2971837355@qq.com>
2022-08-28 12:07:57 +0800

    重置

--- docs/.vuepress/.temp/register-components/clientConfig.c193fd9c.js
+++
@@ -1,9 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export default {
-  enhance: ({ app }) => {    
-      app.component("catDazi", defineAsyncComponent(() => import("E:/save/Vuepress/docs/.vuepress/components/catDazi.vue"))),
-      app.component("Sakura", defineAsyncComponent(() => import("E:/save/Vuepress/docs/.vuepress/components/Sakura.vue"))),
-      app.component("video", defineAsyncComponent(() => import("E:/save/Vuepress/docs/.vuepress/components/video.vue")))
-  },
-}
--- docs/.vuepress/components/video.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <video src="./public/video/项目演示.mp4"></video>
-</template>
